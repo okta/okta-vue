6046af9edbb4fbcae1ae49e9e7849e792bd821e2
Aaron Granick <aaron.granick@okta.com>
2020-05-28 15:38:53 -0700

    initial commit

---
+++ packages/okta-vue/jest.config.js
@@ -0,0 +1,24 @@
+var ENV = require('../../env')()
+
+module.exports = {
+  'globals': {
+    'PACKAGE': ENV.packageInfo
+  },
+  'restoreMocks': true,
+  'moduleFileExtensions': [
+    'js',
+    'json',
+    'vue'
+  ],
+  'transform': {
+    '^.+\\.js$': '<rootDir>/../../node_modules/babel-jest',
+    '.*\\.(vue)$': '<rootDir>/../../node_modules/vue-jest'
+  },
+  'moduleNameMapper': {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  'roots': [
+    'test/specs'
+  ],
+  'testEnvironment': 'jsdom'
+}
---
+++ scripts/publish.sh
@@ -0,0 +1,50 @@
+#!/bin/bash -xe
+
+source ${OKTA_HOME}/${REPO}/scripts/setup.sh
+
+REGISTRY="${ARTIFACTORY_URL}/api/npm/npm-okta"
+
+export TEST_SUITE_TYPE="build"
+
+# Install required dependencies
+export PATH="${PATH}:$(yarn global bin)"
+yarn global add @okta/ci-update-package
+yarn global add @okta/ci-pkginfo
+
+if [ -n "${action_branch}" ];
+then
+  echo "Publishing from bacon task using branch ${action_branch}"
+  TARGET_BRANCH=${action_branch}
+else
+  echo "Publishing from bacon testSuite using branch ${BRANCH}"
+  TARGET_BRANCH=${BRANCH}
+fi
+
+# Copy README, CHANGELOG, and CONTRIBUTING to the package directory so they will be published with the the package on npm
+cp ./*.md ./packages/okta-vue/
+
+pushd ./packages/okta-vue
+
+if ! ci-update-package --branch ${TARGET_BRANCH}; then
+  echo "ci-update-package failed! Exiting..."
+  exit ${FAILED_SETUP}
+fi
+
+### looks like ci-update-package is not compatible with `yarn publish`
+### which expects new-version is passed via command line parameter.
+### keep using npm for now
+if ! npm publish --registry ${REGISTRY}; then
+  echo "npm publish failed! Exiting..."
+  exit ${PUBLISH_ARTIFACTORY_FAILURE}
+fi
+
+
+DATALOAD=$(ci-pkginfo -t dataload)
+if ! artifactory_curl -X PUT -u ${ARTIFACTORY_CREDS} ${DATALOAD} -v -f; then
+  echo "artifactory_curl failed! Exiting..."
+  exit ${PUBLISH_ARTIFACTORY_FAILURE}
+fi
+
+popd
+
+exit ${SUCCESS}
