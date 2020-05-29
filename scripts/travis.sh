6ea38a6156f9950da5dddb5ce178c4d83b1f9f09
Aaron Granick <aaron.granick@okta.com>
2020-05-29 16:05:42 -0700

    Update travis.sh

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -4,7 +4,6 @@ if [ "${TRAVIS_EVENT_TYPE}" = "cron" ] ; then
     export RUN_SAUCE_TESTS=true;
     yarn test:e2e
 else
-    # run the lint and e2e tests (on chrome headless)
-    yarn lint
+    # run the lint, unit and e2e tests (on chrome headless)
     yarn test
 fi
