ffdf6ca05edc005136aacf39fb9e60e03c44afde
Jared Perreault <jared.perreault@okta.com>
2022-01-19 16:59:05 +0000

    adds eslint import/no-extraneous-dependencies rule
    OKTA-460199
    <<<Jenkins Check-In of Tested SHA: 5d6edcecc951afd66ad948986cc7c737e8d48691 for eng_productivity_ci_bot_okta@okta.com>>>
    Artifact: okta-vue
    Files changed count: 3
    PR Link: "https://github.com/okta/okta-vue/pull/84"

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -9,5 +9,21 @@ module.exports = {
     'plugin:vue/essential',
     'eslint:recommended',
     '@vue/typescript/recommended'
-  ]
+  ],
+  plugins: [
+    // https://github.com/import-js/eslint-plugin-import#typescript
+    'import',
+  ],
+  rules: {
+    // https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/no-extraneous-dependencies.md
+    'import/no-extraneous-dependencies': ['error', {
+      'devDependencies': false
+    }]
+  },
+  settings: {
+    // https://github.com/import-js/eslint-plugin-import#typescript
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts']
+    }
+  },
 };
