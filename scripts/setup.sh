7cd3c2cf47c8ec2d7c09565a9988d827ed1ca1bd
Jared Perreault <jared.perreault@okta.com>
2022-08-23 14:03:19 -0400

    enable auth-js --> okta-vue downstream

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -36,7 +36,8 @@ OKTA_REGISTRY=${ARTIFACTORY_URL}/api/npm/npm-okta-master
 echo "Replacing $YARN_REGISTRY with $OKTA_REGISTRY within yarn.lock files..."
 sed -i "s#${YARN_REGISTRY}#${OKTA_REGISTRY}#" yarn.lock
 
-if ! yarn install ; then
+# Install dependencies but do not build
+if ! yarn install --frozen-lockfile --ignore-scripts; then
   echo "yarn install failed! Exiting..."
   exit ${FAILED_SETUP}
 fi
@@ -44,3 +45,37 @@ fi
 # Revert the original change(s)
 echo "Replacing $OKTA_REGISTRY with $YARN_REGISTRY within yarn.lock files..."
 sed -i "s#${OKTA_REGISTRY}#${YARN_REGISTRY}#" yarn.lock
+
+# Install a specific version of auth-js, used by downstream artifact builds
+if [ ! -z "$AUTHJS_VERSION" ]; then
+  echo "Installing AUTHJS_VERSION: ${AUTHJS_VERSION}"
+  npm config set strict-ssl false
+
+  AUTHJS_URI=https://artifacts.aue1d.saasure.com/artifactory/npm-topic/@okta/okta-auth-js/-/@okta/okta-auth-js-${AUTHJS_VERSION}.tgz
+  if ! yarn add -DW --ignore-scripts ${AUTHJS_URI}; then
+    echo "AUTHJS_VERSION could not be installed: ${AUTHJS_VERSION}"
+    exit ${FAILED_SETUP}
+  fi
+
+  # NOTE: no additional auth-js versions are included in this repo. If this changes ensure the downstream auth-js is used in all test envs/apps
+  # Example here: https://github.com/okta/okta-react/blob/58883b02f95aeb21befe61ae60e11e4591c792d6/scripts/setup.sh#L69
+
+  npm config set strict-ssl true
+  echo "AUTHJS_VERSION installed: ${AUTHJS_VERSION}"
+
+  # verify single version of auth-js is installed
+  # NOTE: okta-signin-widget will install it's own version of auth-js, filtered out
+  AUTHJS_INSTALLS=$(find . -type d -path "*/node_modules/@okta/okta-auth-js" -not -path "*/okta-signin-widget/*" | wc -l)
+  if [ $AUTHJS_INSTALLS -gt 1 ]
+  then
+    echo "ADDITIONAL AUTH JS INSTALL DETECTED"
+    yarn why @okta/okta-auth-js
+    exit ${FAILED_SETUP}
+  fi
+fi
+
+# build okta-vue
+if ! yarn build; then
+  echo "yarn build failed! Exiting..."
+  exit ${FAILED_SETUP}
+fi
