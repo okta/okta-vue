141b02e27daf94b922fcfc48fedf637ae5a06d81
denys.oblohin <denys.oblohin@okta.com>
2022-09-05 19:42:19 +0300

    Fix warning about missing root route

---
+++ test/app/src/components/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+  </div>
+</template>
+  
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Home'
+})
+</script>
--- test/app/src/router/index.ts
+++ test/app/src/router/index.ts
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { LoginCallback, navigationGuard } from '@okta/okta-vue'
 import Protected from '@/components/Protected.vue';
+import Home from '@/components/Home.vue';
 import SessionTokenLogin from '@/components/SessionTokenLogin.vue';
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes: [
+    { path: '/', component: Home },
     { path: '/login/callback', component: LoginCallback },
     { path: '/protected', component: Protected, meta: { requiresAuth: true } },
     { path: '/sessionToken', component: SessionTokenLogin }
