5cd7a25b0ef81098380f64ca2b02b64cde00d6d2
denys.oblohin <denys.oblohin@okta.com>
2023-06-13 13:09:24 +0300

    get_vault_secret_key

--- scripts/e2e-v6.sh
+++ scripts/e2e-v6.sh
@@ -17,7 +17,7 @@ export CI=true
 export ISSUER=https://samples-javascript.okta.com/oauth2/default
 export SPA_CLIENT_ID=0oapmwm72082GXal14x6
 export USERNAME=george@acme.com
-get_secret prod/okta-sdk-vars/password PASSWORD
+get_vault_secret_key devex/samples-javascript password PASSWORD
 
 if ! yarn add -DW --ignore-scripts @okta/okta-auth-js@^6; then
   echo "auth-js v6.x could not be installed"
