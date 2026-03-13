c269e9f08ccf89e03ff644e9bd392e3bcc7e4118
Jared Perreault <90656038+jaredperreault-okta@users.noreply.github.com>
2026-03-13 15:12:57 -0400

    bumps okta-auth-js version range (#151)
    OKTA-1130668 bumps okta-auth-js version range

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -11,7 +11,7 @@ export AUTHJS_VERSION=""
 
 # Install required node version
 export NVM_DIR="/root/.nvm"
-setup_service node v14.19.3
+setup_service node v16.20.2
 
 # Install yarn
 # Use the cacert bundled with centos as okta root CA is self-signed and cause issues downloading from yarn
